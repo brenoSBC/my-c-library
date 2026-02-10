191e1ba39914d3989eb3ffa6de65225dc3e54a65
brenoSBC <sbcbreno01@gmail.com>
2026-02-10 16:25:05 -0300

    initial logic

--- memmove.c
+++ memmove.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void *my_memmove(void *dest, void *src, int n)
+void *my_memmove(void *dest, const void *src, size_t n)
 {
 
-    unsigned char *pdest = (char *)dest;
-    const unsigned char *psrc = (char *)src;
+    if(dest == src || n == 0) {
+        return dest;
+    }
+
+    unsigned char *pdest = dest;
+    const unsigned char *psrc = src;
 
     if ((uintptr_t)pdest > (uintptr_t)psrc && (uintptr_t)pdest < (uintptr_t)psrc + n)
     {
+        unsigned char *last_dest = pdest + (n - 1);
+        unsigned char *last_src = psrc + (n - 1);
 
+        while(n != 0) {
+            *last_dest = *last_src;
+            --last_dest;
+            --last_src;
+            --n;
+        }
     }
     else
     {
-
+        while(n != 0) {
+            *pdest = *psrc;
+            ++pdest;
+            ++psrc;
+            --n;
+        }
     }
 
     return dest;
